62023aad4dcfd1b2707c8dbea05edefec1f63bf1
hyang336 <haozi078@gmail.com>
2024-01-15 02:42:44 -0800

    Update results.m
    added plots for mean and se for each of the metrics.
    
    also saving T-val based time estimates in the output csv

--- results.m
+++ results.m
@@ -11,6 +11,10 @@ num_left=floor(((bin_num-1)/zoom_factor)/2);
 num_right=num_left;
 median_order=8;
 
+% Make output dir
+if ~exist(strcat(output_dir,'/figures'),'dir')
+    mkdir(strcat(output_dir,'/figures'));
+end
 
 %ROI mask
 if ~strcmp(mask,'whole')
@@ -48,8 +52,8 @@ for i=1:length(SSID)
         
         if ~isempty(files)
 
-            volume_sum_res=[];
-            volume_std_res=[];
+            volume_mean_res=[];
+            volume_se_res=[];
             if ~strcmp(mask,'whole')
                 ROI=find(brod==17);
             else
@@ -91,54 +95,119 @@ for i=1:length(SSID)
                     ROItrind=trind_vol;
                     ROItrmid=trmid_vol;
                 end
-                volume_sum_res(k)=sum(sum(sum(ROIres,'omitnan'),'omitnan'),'omitnan');
-                volume_std_res(k)=std(ROIres,1,'all','omitnan');
+                volume_mean_res(k)=(sum(sum(sum(ROIres,'omitnan'),'omitnan'),'omitnan'))/sum(sum(sum(~isnan(ROIres))));
+                volume_se_res(k)=std(ROIres,1,'all','omitnan')/sqrt(sum(sum(sum(~isnan(ROIres)))));
 
-                volume_sum_tgoal(k)=sum(sum(sum(ROItgoal,'omitnan'),'omitnan'),'omitnan');
-                volume_std_tgoal(k)=std(ROItgoal,1,'all','omitnan');
+                volume_mean_tgoal(k)=sum(sum(sum(ROItgoal,'omitnan'),'omitnan'),'omitnan')/sum(sum(sum(~isnan(ROItgoal))));
+                volume_se_tgoal(k)=std(ROItgoal,1,'all','omitnan')/sqrt(sum(sum(sum(~isnan(ROItgoal)))));
 
-                volume_sum_tstim(k)=sum(sum(sum(ROItstim,'omitnan'),'omitnan'),'omitnan');
-                volume_std_tstim(k)=std(ROItstim,1,'all','omitnan');
+                volume_mean_tstim(k)=sum(sum(sum(ROItstim,'omitnan'),'omitnan'),'omitnan')/sum(sum(sum(~isnan(ROItstim))));
+                volume_se_tstim(k)=std(ROItstim,1,'all','omitnan')/sqrt(sum(sum(sum(~isnan(ROItstim)))));
 
-                volume_sum_trind(k)=sum(sum(sum(ROItrind,'omitnan'),'omitnan'),'omitnan');
-                volume_std_trind(k)=std(ROItrind,1,'all','omitnan');
+                volume_mean_trind(k)=sum(sum(sum(ROItrind,'omitnan'),'omitnan'),'omitnan')/sum(sum(sum(~isnan(ROItrind))));
+                volume_se_trind(k)=std(ROItrind,1,'all','omitnan')/sqrt(sum(sum(sum(~isnan(ROItrind)))));
 
-                volume_sum_trmid(k)=sum(sum(sum(ROItrmid,'omitnan'),'omitnan'),'omitnan');
-                volume_std_trmid(k)=std(ROItrmid,1,'all','omitnan');
+                volume_mean_trmid(k)=sum(sum(sum(ROItrmid,'omitnan'),'omitnan'),'omitnan')/sum(sum(sum(~isnan(ROItrmid))));
+                volume_se_trmid(k)=std(ROItrmid,1,'all','omitnan')/sqrt(sum(sum(sum(~isnan(ROItrmid)))));
             end
             %%
-            %plot and save the ROI residual for the current run
+            %plot and save the ROI residual for the current run            
+            figure;
+            errorbar(tile,volume_mean_res,volume_se_res,'b', 'LineWidth', 1.5);
+            title('Mean volumne residual with standard error');
+            xlabel('time shift');
+            ylabel('Volume-mean residual');
+            xticks(linspace(s_time, e_time, 11));
+            saveas(gcf,[output_dir,'/figures/',SSID{i},'_',runs{j},'_res_plot.png'])
+            close;
             
+            figure;
+            errorbar(tile,volume_mean_tgoal,volume_se_tgoal,'b', 'LineWidth', 1.5);
+            title('Mean volumne T-value for goal>baseline with standard error');
+            xlabel('time shift');
+            ylabel('Volume-mean T-value');
+            xticks(linspace(s_time, e_time, 11));
+            saveas(gcf,[output_dir,'/figures/',SSID{i},'_',runs{j},'_tgoal_plot.png'])
+            close;
+
+            figure;
+            errorbar(tile,volume_mean_tstim,volume_se_tstim,'b', 'LineWidth', 1.5);
+            title('Mean volumne T-value for stim>baseline with standard error');
+            xlabel('time shift');
+            ylabel('Volume-mean T-value');
+            xticks(linspace(s_time, e_time, 11));
+            saveas(gcf,[output_dir,'/figures/',SSID{i},'_',runs{j},'_tstim_plot.png'])
+            close;
+
+            figure;
+            errorbar(tile,volume_mean_trind,volume_se_trind,'b', 'LineWidth', 1.5);
+            title('Mean volumne T-value for right_index>baseline with standard error');
+            xlabel('time shift');
+            ylabel('Volume-mean T-value');
+            xticks(linspace(s_time, e_time, 11));
+            saveas(gcf,[output_dir,'/figures/',SSID{i},'_',runs{j},'_trind_plot.png'])
+            close;
+
+            figure;
+            errorbar(tile,volume_mean_trmid,volume_se_trmid,'b', 'LineWidth', 1.5);
+            title('Mean volumne T-value for right_middle>baseline with standard error');
+            xlabel('time shift');
+            ylabel('Volume-mean T-value');
+            xticks(linspace(s_time, e_time, 11));
+            saveas(gcf,[output_dir,'/figures/',SSID{i},'_',runs{j},'_trmid_plot.png'])
+            close;
 
             %global min
-            res_min_ind=find(volume_sum_res==min(volume_sum_res));
-            res_min_std_ind=find(volume_std_res==min(volume_std_res));
+            res_min_ind=find(volume_mean_res==min(volume_mean_res));
+            res_min_se_ind=find(volume_se_res==min(volume_se_res));
+            
+            %global max for t value, but still looking for min std
+            tgoal_max_ind=find(volume_mean_tgoal==max(volume_mean_tgoal));
+            tgoal_min_se_ind=find(volume_se_tgoal==min(volume_se_tgoal));
+
+            tstim_max_ind=find(volume_mean_tstim==max(volume_mean_tstim));
+            tstim_min_std_ind=find(volume_se_tstim==min(volume_se_tstim));
+            
+            trind_max_ind=find(volume_mean_trind==max(volume_mean_trind));
+            trind_min_std_ind=find(volume_se_trind==min(volume_se_trind));
+
+            trmid_max_ind=find(volume_mean_trmid==max(volume_mean_trmid));
+            trmid_min_std_ind=find(volume_se_trmid==min(volume_se_trmid));
 
             %check if the min or max is a locally smooth peak
-            is_smooth_peak=SmoothPeakDetect(volume_sum_res,'min',res_min_ind,num_left);
+            is_smooth_peak=SmoothPeakDetect(volume_mean_res,'min',res_min_ind,num_left);
 
 
             %if not a smooth peak, smooth
             if is_smooth_peak
-                volume_sum_res_smooth=volume_sum_res;
+                volume_sum_res_smooth=volume_mean_res;
                 
             else
-                volume_sum_res_smooth=medfilt1(volume_sum_res,median_order,'omitnan','truncate');
+                volume_sum_res_smooth=medfilt1(volume_mean_res,median_order,'omitnan','truncate');
                
             end
             
             res_min_ind_smooth=find(volume_sum_res_smooth==min(volume_sum_res_smooth));
             
-            time_est_res=tile(res_min_ind);
-            time_est_smooth_res=tile(res_min_ind_smooth);
-            time_est_res_std=tile(res_min_std_ind);
+            %peak-based time estimates
+            time_est_res=tile(res_min_ind);%minimal mean residual
+            time_est_smooth_res=tile(res_min_ind_smooth);%minimal smoothed mean residual
+            time_est_res_se=tile(res_min_se_ind);%minimal standard error
+            
+            time_est_tgoal=tile(tgoal_max_ind);
+            time_est_tstim=tile(tstim_max_ind);
+            time_est_trind=tile(trind_max_ind);
+            time_est_trmid=tile(trmid_max_ind);
 
             %save smoothed and nonsmoothed global min
-            rowdata1={time_est_res,runs{j},SSID{i},'VolResSum'};
-            rowdata2={time_est_smooth_res(1),runs{j},SSID{i},'VolResSum_smooth'};%just take the 1st component since the smoothing may give min on multiple time points
-            rowdata3={time_est_res_std,runs{j},SSID{i},'VolResStd'};
-
-            comp_table=[comp_table;rowdata1;rowdata2;rowdata3];
+            rowdata1={time_est_res,runs{j},SSID{i},'VolResMean'};
+            rowdata2={time_est_smooth_res(1),runs{j},SSID{i},'VolResMean_smooth'};%just take the 1st component since the smoothing may give min on multiple time points
+            rowdata3={time_est_res_se,runs{j},SSID{i},'VolResSe'};
+            rowdata4={time_est_tgoal,runs{j},SSID{i},'VolGoalTvalMean'};
+            rowdata5={time_est_tstim,runs{j},SSID{i},'VolStimTvalMean'};
+            rowdata6={time_est_trind,runs{j},SSID{i},'VolRIndTvalMean'};
+            rowdata7={time_est_trmid,runs{j},SSID{i},'VolRMidTvalMean'};
+            comp_table=[comp_table;rowdata1;rowdata2;rowdata3;rowdata4;rowdata5;rowdata6;rowdata7];
         end
     end
     disp(i)
